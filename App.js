3f78e6e80562e998005196c4292ba4037c190005
Tocoro10 <haruo512@yahoo.co.jp>
2018-12-11 20:46:59 +0900

    create Login and Aignin

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 
 export default class App extends React.Component {
@@ -12,7 +13,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoEditScreen/>
+        <LoginScreen/>
       </View>
     );
   }
