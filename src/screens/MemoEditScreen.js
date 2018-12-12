6c4e8fa9349fbf5f9fa7ed11a178b8469a716b88
Tocoro10 <haruo512@yahoo.co.jp>
2018-12-12 20:19:40 +0900

    Hide firebase certificate

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import {
-    StyleSheet,
-    View,
-    TextInput
-}from 'react-native';
+    import React from 'react';
+    import {
+        StyleSheet,
+        View,
+        TextInput
+    }from 'react-native';
 
-import CircleButton from '../elements/CircleButton';
+    import CircleButton from '../elements/CircleButton';
 
-class MemoEditScreen extends React.Component{
-    render(){
-        return(
-            <View style={styles.container}>
-                <TextInput style={styles.memoEditInput} multiline value="Hi"/>
-                <CircleButton name ="check" />
-            </View>
-        );
+    class MemoEditScreen extends React.Component{
+        render(){
+            return(
+                <View style={styles.container}>
+                    <TextInput style={styles.memoEditInput} multiline value="Hi"/>
+                    <CircleButton name ="check" onPress={()=>{this.props.navigation.goBack()}}/>
+                </View>
+            );
+        }
     }
-}
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        width:'100%',
-    },
-    memoEditInput:{
-        backgroundColor:'#fff',
-        flex:1,
-        paddingTop:32,
-        paddingLeft:16,
-        paddingRight:16,
-        paddingBottom:16,
-        fontSize:16,
-    },
-});
+    const styles = StyleSheet.create({
+        container:{
+            flex:1,
+            width:'100%',
+        },
+        memoEditInput:{
+            backgroundColor:'#fff',
+            flex:1,
+            paddingTop:32,
+            paddingLeft:16,
+            paddingRight:16,
+            paddingBottom:16,
+            fontSize:16,
+        },
+    });
 
-export default MemoEditScreen;
+    export default MemoEditScreen;
